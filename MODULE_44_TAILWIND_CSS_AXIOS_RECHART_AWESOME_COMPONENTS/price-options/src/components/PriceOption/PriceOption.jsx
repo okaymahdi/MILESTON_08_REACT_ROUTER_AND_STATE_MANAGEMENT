42eaac2c9927c9c97ca2c6d6e401113991237b00
okaymahdi <mahdimunshee@gmail.com>
2024-03-21 23:46:25 +0600

    added PriceOptions, PriceOption

--- MODULE_44_TAILWIND_CSS_AXIOS_RECHART_AWESOME_COMPONENTS/price-options/src/components/PriceOption/PriceOption.jsx
+++ MODULE_44_TAILWIND_CSS_AXIOS_RECHART_AWESOME_COMPONENTS/price-options/src/components/PriceOption/PriceOption.jsx
@@ -1,14 +1,23 @@
 import PropTypes from 'prop-types';
+import Feature from '../Features/Feature';
 
 const PriceOption = ({ option }) => {
     const { name, price, features } = option;
     return (
-        <div>
-            <h2>
-                <span className='text-7xl'>{price}</span>
+        <div className='flex flex-col bg-blue-600 rounded-lg p-4 text-white'>
+            <h2 className='text-center'>
+                <span className='text-7xl font-extrabold'>{price}</span>
                 <span className='text-2xl'>/month</span>
             </h2>
-            <h4 className='text-3xl'>{name}</h4>
+            <h4 className='text-3xl text-center my-8'>{name}</h4>
+
+            {/* features */}
+            <div className='pl-6 flex-grow'>
+                {
+                    features.map((feature, index) => <Feature key={index} feature={feature}></Feature>)
+                }
+            </div>
+            <button className='mt-12 w-full py-2 bg-green-600 font-bold rounded-lg hover:bg-green-900'>Bay Now</button>
         </div>
     );
 };
--- MODULE_44_TAILWIND_CSS_AXIOS_RECHART_AWESOME_COMPONENTS/price-options/src/components/PriceOptions/PriceOptions.jsx
+++ MODULE_44_TAILWIND_CSS_AXIOS_RECHART_AWESOME_COMPONENTS/price-options/src/components/PriceOptions/PriceOptions.jsx
@@ -1,51 +1,54 @@
 import PriceOption from "../PriceOption/PriceOption";
 
 const PriceOptions = () => {
-    [
-        {
-            "id": 1,
-            "name": "Basic",
-            "price": 29.99,
-            "features": [
-                "Access to cardio and weightlifting areas",
-                "Limited group fitness classes (2 per week)",
-                "No access to sauna or swimming pool",
-                "Discounts on gym merchandise"
-            ]
-        },
-        {
-            "id": 2,
-            "name": "Standard",
-            "price": 49.99,
-            "features": [
-                "Access to cardio, weightlifting, and group fitness areas",
-                "Unlimited group fitness classes",
-                "Limited access to sauna",
-                "Access to swimming pool during designated hours",
-                "Locker rental option"
-            ]
-        },
-        {
-            "id": 3,
-            "name": "Premium",
-            "price": 79.99,
-            "features": [
-                "Full access to all gym facilities including sauna and swimming pool",
-                "Unlimited group fitness classes",
-                "Discounts on personal training sessions",
-                "Free towel service",
-                "Complimentary protein shakes"
-            ]
-        }
-    ]
+    const PriceOptions =
+        [
+            {
+                "id": 1,
+                "name": "Basic",
+                "price": 29.99,
+                "features": [
+                    "Access to cardio and weightlifting areas",
+                    "Limited group fitness classes (2 per week)",
+                    "No access to sauna or swimming pool",
+                    "Discounts on gym merchandise"
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Standard",
+                "price": 49.99,
+                "features": [
+                    "Access to cardio, weightlifting",
+                    "Unlimited group fitness classes",
+                    "Limited access to sauna",
+                    "Access to swimming pool during designated hours",
+                    "Locker rental option"
+                ]
+            },
+            {
+                "id": 3,
+                "name": "Premium",
+                "price": 79.99,
+                "features": [
+                    "Full access to all gym facilities",
+                    "Unlimited group fitness classes",
+                    "Discounts on personal training sessions",
+                    "Free towel service",
+                    "Complimentary protein shakes"
+                ]
+            }
+        ]
 
 
     return (
-        <div>
+        <div className="m-12">
             <h2 className="text-5xl">Best Prices in the Town</h2>
-            {
-                PriceOptions.map(option => <PriceOption key={option.id} option={option}></PriceOption>)
-            }
+            <div className="grid md:grid-cols-3 gap-6">
+                {
+                    PriceOptions.map(option => <PriceOption key={option.id} option={option}></PriceOption>)
+                }
+            </div>
         </div>
     );
 };
