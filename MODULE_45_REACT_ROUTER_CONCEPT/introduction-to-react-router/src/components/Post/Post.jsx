6023003b58927b176d28eaf479771d54077c47b1
okaymahdi <mahdimunshee@gmail.com>
2024-03-24 02:23:01 +0600

    added errorHandler

--- MODULE_45_REACT_ROUTER_CONCEPT/introduction-to-react-router/src/components/Post/Post.jsx
+++ MODULE_45_REACT_ROUTER_CONCEPT/introduction-to-react-router/src/components/Post/Post.jsx
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({ post }) => {
     const { id, title } = post;
+
+    const navigate = useNavigate();
 
     const postStyle = {
         border: '2px solid yellow',
         padding: '15px',
         borderRadius: '20px',
+    }
+
+    const handleShowDetail = () => {
+        navigate(`/post/${id}`);
     }
     return (
         <div style={postStyle}>
@@ -14,6 +20,8 @@ const Post = ({ post }) => {
             <p>Title: {title}</p>
             <Link to={`/post/${id}`}>Post Detail</Link>
             <Link to={`/post/${id}`}><button>Show Details</button></Link>
+
+            <button onClick={handleShowDetail}>Click to Show Details</button>
         </div>
     );
 };
--- MODULE_45_REACT_ROUTER_CONCEPT/introduction-to-react-router/src/components/PostDetails/PostDetails.jsx
+++ MODULE_45_REACT_ROUTER_CONCEPT/introduction-to-react-router/src/components/PostDetails/PostDetails.jsx
@@ -1,13 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
     const post = useLoaderData();
     const {id, title, body} = post;
+
+    const {postId} = useParams();
+    console.log(postId);
+
+    const navigate = useNavigate();
+    const handelGoBack = () => {
+        navigate(-1)
+    }
     return (
         <div>
             <h3>Post details About: {id}</h3>
             <p>Title: {title}</p>
             <p><small>Body: {body}</small></p>
+            <button onClick={handelGoBack}>Go back</button>
         </div>
     );
 };
