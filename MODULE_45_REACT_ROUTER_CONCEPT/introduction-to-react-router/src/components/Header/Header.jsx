37273654867fdc2048a8f48e65ed961c58db9d9a
okaymahdi <mahdimunshee@gmail.com>
2024-03-24 02:53:38 +0600

    updated

--- MODULE_45_REACT_ROUTER_CONCEPT/introduction-to-react-router/src/components/Header/Header.jsx
+++ MODULE_45_REACT_ROUTER_CONCEPT/introduction-to-react-router/src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
 /** connect CSS */
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
         <div>
             <span>My Website</span>
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/users">Users</Link>
-                <Link to="/posts">Posts</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact Us</Link>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/users">Users</NavLink>
+                <NavLink to="/posts">Posts</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/contact">Contact Us</NavLink>
             </nav>
         </div>
     );
